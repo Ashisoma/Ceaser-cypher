5ca1023306d56b4cf23952e91341908f9ea8970e
Ashisoma <okalionesmu@gmail.com>
2020-10-10 08:15:59 +0300

    Add new java app to github

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
+import java.io.Console;
+
 public class App {
     public static void main(String[] args) {
+        Console myConsole = System.console();
+        String inputText = "Hello";
+        System.out.print(inputText);
 
+        char [] chars =  inputText.toCharArray();
+         for (char c : chars) {
+             c +=1;
+             System.out.print(c);
+         }
     }
 }
